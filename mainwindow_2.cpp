34fbfb8722586a75dd419ed6402931a79b957f0e
Vadimkkaa <70854828+Vadimkkaa@users.noreply.github.com>
2022-05-09 22:00:35 +0300

    Commit

--- mainwindow_2.cpp
+++ mainwindow_2.cpp
@@ -52,7 +52,7 @@ void MainWindow_2::on_pushButton_3_clicked()
         QString file = dir.absoluteFilePath("C:/Program Files/Git/git-bash.exe");
         process->start(file);
 
-        Sleep(5000);
+        Sleep(4000);
         QMessageBox::information(this,"Upload progress","Done!");
 
     }
